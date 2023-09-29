332a61de700246e7911f797cdc09a1e0c980da29
nanidam <nani.dam@medieinstitutet.se>
2023-09-29 18:09:41 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
     exclude: ["@digi/arbetsformedlingen"],
   },
-  base: '/case-af-jobtech/',
+  base: "/Case-AF-with-JobTech/",
   plugins: [react()],
-})
+});
